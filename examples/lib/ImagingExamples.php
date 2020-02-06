d8d0338596eb0c343a815a15d51c27c509ad3be0
Sergei Zubov <sergei.zubov@aspose.com>
2020-02-06 17:55:57 +0300

    Enable examples

--- examples/lib/ImagingExamples.php
+++ examples/lib/ImagingExamples.php
@@ -61,160 +61,164 @@ $appSid = null;
 $baseUrl = null;
 processArguments($argv, $appKey, $appSid, $baseUrl);
 
-$imagingConfig = new Configuration();
-$imagingConfig->setAppKey($appKey);
-$imagingConfig->setAppSid($appSid);
-$imagingConfig->setBaseUrl($baseUrl);
-$imagingApi = new ImagingApi($imagingConfig);
+try {
+    $imagingConfig = new Configuration();
+    $imagingConfig->setAppKey($appKey);
+    $imagingConfig->setAppSid($appSid);
+    $imagingConfig->setBaseUrl($baseUrl);
+    $imagingApi = new ImagingApi($imagingConfig);
 
-if (file_exists(ImagingBase::GetOutputFolder()))
-    deleteDir(ImagingBase::GetOutputFolder());
-mkdir(ImagingBase::GetOutputFolder());
+    if (file_exists(ImagingBase::GetOutputFolder()))
+        deleteDir(ImagingBase::GetOutputFolder());
+    mkdir(ImagingBase::GetOutputFolder());
 
-echo "Running Imaging Cloud examples:" . PHP_EOL;
+    echo "Running Imaging Cloud examples:" . PHP_EOL;
 
-//// Update parameters of existing BMP image
-//$bmpImage = new UpdateBmpImage($imagingApi);
-//$bmpImage->ModifyBmpFromStorage();
-//$bmpImage->ModifyBmpAndUploadToStorage();
-//$bmpImage->CreateModifiedBmpFromRequestBody();
-//
-//// Crop an existing image
-//$cropImage = new CropImage($imagingApi);
-//$cropImage->CropImageFromStorage();
-//$cropImage->CropImageAndUploadToStorage();
-//$cropImage->CreateCroppedImageFromRequestBody();
-//
-//// Process existing EMF imaging using given parameters
-//$updateEMFImage = new UpdateEmfImage($imagingApi);
-//$updateEMFImage->ModifyEmfFromStorage();
-//$updateEMFImage->ModifyEmfAndUploadToStorage();
-//$updateEMFImage->CreateModifiedEmfFromRequestBody();
-//
-//// Export existing image to another format
-//$exportImage = new ExportImage($imagingApi);
-//$exportImage->SaveImageAsFromStorage();
-//$exportImage->SaveImageAsAndUploadToStorage();
-//$exportImage->CreateSavedImageAsFromRequestBody();
-//
-//// Get properties of an image
-//$imageProperties = new ImageProperties($imagingApi);
-//$imageProperties->GetImagePropertiesFromStorage();
-//$imageProperties->ExtractImagePropertiesFromRequestBody();
-//
-//// Resize an existing image
-//$resizeImage = new ResizeImage($imagingApi);
-//$resizeImage->ResizeImageFromStorage();
-//$resizeImage->ResizeImageAndUploadToStorage();
-//$resizeImage->CreateResizedImageFromRequestBody();
-//
-//// Rotate and/or flip an existing image
-//$rotateFlipImage = new RotateFlipImage($imagingApi);
-//$rotateFlipImage->RotateFlipImageFromStorage();
-//$rotateFlipImage->RotateFlipImageAndUploadToStorage();
-//$rotateFlipImage->CreateRotateFlippedImageFromRequestBody();
-//
-//// TIFF Frames
-//$tiffFrames = new TIFFFrames($imagingApi);
-//// Get a specified frame from existing TIFF image
-//$tiffFrames->GetImageFrameFromStorage();
-//// Get a specified frame from existing TIFF image, and upload the frame to Cloud Storage
-//$tiffFrames->GetImageFrameAndUploadToStorage();
-//// Resize a TIFF frame
-//$tiffFrames->ResizeImageFrameFromStorage();
-//// Crop a TIFF frame
-//$tiffFrames->CropImageFrameFromStorage();
-//// RotateFlip a TIFF frame
-//$tiffFrames->RotateFlipImageFrameFromStorage();
-//// Get other frames from existing TIFF image
-//$tiffFrames->GetAllImageFramesFromStorage();
-//// Get separate frame from existing TIFF image.
-//// Image data is passed as zero-indexed multipart/form-data content or as raw body stream
-//$tiffFrames->CreateImageFrameFromRequestBody();
-//// Get separate frame properties of existing TIFF image
-//$tiffFrames->GetImageFramePropertiesFromStorage();
-//// Get separate frame properties of existing TIFF image.
-//// Image data is passed as zero-indexed multipart/form-data content or as raw body stream
-//$tiffFrames->ExtractImageFramePropertiesFromRequestBody();
-//
-//// Update parameters of existing TIFF image
-//$tiffImage = new UpdateTiffImage($imagingApi);
-//$tiffImage->ModifyTiffFromStorage();
-//$tiffImage->ModifyTiffAndUploadToStorage();
-//$tiffImage->CreateModifiedTiffFromRequestBody();
-//$tiffImage->ConvertTiffToFaxFromStorage();
-//$tiffImage->AppendTiffFromStorage();
-//
-//// Update parameters of existing GIF image
-//$updateGIFImage = new UpdateGifImage($imagingApi);
-//$updateGIFImage->ModifyGifFromStorage();
-//$updateGIFImage->ModifyGifAndUploadToStorage();
-//$updateGIFImage->CreateModifiedGifFromRequestBody();
-//
-//// Perform scaling, cropping and flipping of an existing image in a single request
-//$updateImage = new UpdateImage($imagingApi);
-//$updateImage->UpdateImageFromStorage();
-//$updateImage->UpdateImageAndUploadToStorage();
-//$updateImage->CreateUpdatedImageFromRequestBody();
-//
-//// Update parameters of existing JPEG2000 image
-//$updateJPEG2000Image = new UpdateJpeg2000Image($imagingApi);
-//$updateJPEG2000Image->ModifyJpeg2000FromStorage();
-//$updateJPEG2000Image->ModifyJpeg2000AndUploadToStorage();
-//$updateJPEG2000Image->CreateModifiedJpeg2000FromRequestBody();
-//
-//// Update parameters of existing JPEG image
-//$updateJPEGImage = new UpdateJpegImage($imagingApi);
-//$updateJPEGImage->ModifyJpegFromStorage();
-//$updateJPEGImage->ModifyJpegAndUploadToStorage();
-//$updateJPEGImage->CreateModifiedJpegFromRequestBody();
-//
-//// Update parameters of existing PSD image
-//$updatePSDImage = new UpdatePsdImage($imagingApi);
-//$updatePSDImage->ModifyPsdFromStorage();
-//$updatePSDImage->ModifyPsdAndUploadToStorage();
-//$updatePSDImage->CreateModifiedPsdFromRequestBody();
-//
-//// Update parameters of existing WEBP image
-//$webpImage = new UpdateWebPImage($imagingApi);
-//$webpImage->ModifyWebPFromStorage();
-//$webpImage->ModifyWebPAndUploadToStorage();
-//$webpImage->CreateModifiedWebPFromRequestBody();
-//
-//// Process existing WMF image using given parameters
-//$wmfImage = new UpdateWmfImage($imagingApi);
-//$wmfImage->ModifyWmfFromStorage();
-//$wmfImage->ModifyWmfAndUploadToStorage();
-//$wmfImage->CreateModifiedWmfFromRequestBody();
-//
-//// Update parameters of existing BMP image
-//$bmpImage = new UpdateBmpImage($imagingApi);
-//$bmpImage->ModifyBmpFromStorage();
-//$bmpImage->ModifyBmpAndUploadToStorage();
-//$bmpImage->CreateModifiedBmpFromRequestBody();
+    // Update parameters of existing BMP image
+    $bmpImage = new UpdateBmpImage($imagingApi);
+    $bmpImage->ModifyBmpFromStorage();
+    $bmpImage->ModifyBmpAndUploadToStorage();
+    $bmpImage->CreateModifiedBmpFromRequestBody();
 
-// AI APIs
-echo "Running AI examples:" . PHP_EOL;
+    // Crop an existing image
+    $cropImage = new CropImage($imagingApi);
+    $cropImage->CropImageFromStorage();
+    $cropImage->CropImageAndUploadToStorage();
+    $cropImage->CreateCroppedImageFromRequestBody();
 
-// Compare two images
-$compareImages = new CompareImages($imagingApi);
-$compareImages->PrepareSearchContext();
-$compareImages->CompareTwoImagesInCloud();
-$compareImages->CompareLoadedImageToImageInCloud();
-$compareImages->DeleteSearchContext();
+    // Process existing EMF imaging using given parameters
+    $updateEMFImage = new UpdateEmfImage($imagingApi);
+    $updateEMFImage->ModifyEmfFromStorage();
+    $updateEMFImage->ModifyEmfAndUploadToStorage();
+    $updateEMFImage->CreateModifiedEmfFromRequestBody();
 
-// Find duplicate images
-$findDuplicateImages = new FindDuplicateImages($imagingApi);
-$findDuplicateImages->PrepareSearchContext();
-$findDuplicateImages->FindImageDuplicates();
-$findDuplicateImages->DeleteSearchContext();
+    // Export existing image to another format
+    $exportImage = new ExportImage($imagingApi);
+    $exportImage->SaveImageAsFromStorage();
+    $exportImage->SaveImageAsAndUploadToStorage();
+    $exportImage->CreateSavedImageAsFromRequestBody();
 
-// Find similar images
-$findSimilarImages = new FindSimilarImages($imagingApi);
-$findSimilarImages->PrepareSearchContext();
-$findSimilarImages->FindImagesSimilar();
-$findSimilarImages->FindImagesByTag();
-$findSimilarImages->DeleteSearchContext();
+    // Get properties of an image
+    $imageProperties = new ImageProperties($imagingApi);
+    $imageProperties->GetImagePropertiesFromStorage();
+    $imageProperties->ExtractImagePropertiesFromRequestBody();
+
+    // Resize an existing image
+    $resizeImage = new ResizeImage($imagingApi);
+    $resizeImage->ResizeImageFromStorage();
+    $resizeImage->ResizeImageAndUploadToStorage();
+    $resizeImage->CreateResizedImageFromRequestBody();
+
+    // Rotate and/or flip an existing image
+    $rotateFlipImage = new RotateFlipImage($imagingApi);
+    $rotateFlipImage->RotateFlipImageFromStorage();
+    $rotateFlipImage->RotateFlipImageAndUploadToStorage();
+    $rotateFlipImage->CreateRotateFlippedImageFromRequestBody();
+
+    // TIFF Frames
+    $tiffFrames = new TIFFFrames($imagingApi);
+    // Get a specified frame from existing TIFF image
+    $tiffFrames->GetImageFrameFromStorage();
+    // Get a specified frame from existing TIFF image, and upload the frame to Cloud Storage
+    $tiffFrames->GetImageFrameAndUploadToStorage();
+    // Resize a TIFF frame
+    $tiffFrames->ResizeImageFrameFromStorage();
+    // Crop a TIFF frame
+    $tiffFrames->CropImageFrameFromStorage();
+    // RotateFlip a TIFF frame
+    $tiffFrames->RotateFlipImageFrameFromStorage();
+    // Get other frames from existing TIFF image
+    $tiffFrames->GetAllImageFramesFromStorage();
+    // Get separate frame from existing TIFF image.
+    // Image data is passed as zero-indexed multipart/form-data content or as raw body stream
+    $tiffFrames->CreateImageFrameFromRequestBody();
+    // Get separate frame properties of existing TIFF image
+    $tiffFrames->GetImageFramePropertiesFromStorage();
+    // Get separate frame properties of existing TIFF image.
+    // Image data is passed as zero-indexed multipart/form-data content or as raw body stream
+    $tiffFrames->ExtractImageFramePropertiesFromRequestBody();
+
+    // Update parameters of existing TIFF image
+    $tiffImage = new UpdateTiffImage($imagingApi);
+    $tiffImage->ModifyTiffFromStorage();
+    $tiffImage->ModifyTiffAndUploadToStorage();
+    $tiffImage->CreateModifiedTiffFromRequestBody();
+    $tiffImage->ConvertTiffToFaxFromStorage();
+    $tiffImage->AppendTiffFromStorage();
+
+    // Update parameters of existing GIF image
+    $updateGIFImage = new UpdateGifImage($imagingApi);
+    $updateGIFImage->ModifyGifFromStorage();
+    $updateGIFImage->ModifyGifAndUploadToStorage();
+    $updateGIFImage->CreateModifiedGifFromRequestBody();
+
+    // Perform scaling, cropping and flipping of an existing image in a single request
+    $updateImage = new UpdateImage($imagingApi);
+    $updateImage->UpdateImageFromStorage();
+    $updateImage->UpdateImageAndUploadToStorage();
+    $updateImage->CreateUpdatedImageFromRequestBody();
+
+    // Update parameters of existing JPEG2000 image
+    $updateJPEG2000Image = new UpdateJpeg2000Image($imagingApi);
+    $updateJPEG2000Image->ModifyJpeg2000FromStorage();
+    $updateJPEG2000Image->ModifyJpeg2000AndUploadToStorage();
+    $updateJPEG2000Image->CreateModifiedJpeg2000FromRequestBody();
+
+    // Update parameters of existing JPEG image
+    $updateJPEGImage = new UpdateJpegImage($imagingApi);
+    $updateJPEGImage->ModifyJpegFromStorage();
+    $updateJPEGImage->ModifyJpegAndUploadToStorage();
+    $updateJPEGImage->CreateModifiedJpegFromRequestBody();
+
+    // Update parameters of existing PSD image
+    $updatePSDImage = new UpdatePsdImage($imagingApi);
+    $updatePSDImage->ModifyPsdFromStorage();
+    $updatePSDImage->ModifyPsdAndUploadToStorage();
+    $updatePSDImage->CreateModifiedPsdFromRequestBody();
+
+    // Update parameters of existing WEBP image
+    $webpImage = new UpdateWebPImage($imagingApi);
+    $webpImage->ModifyWebPFromStorage();
+    $webpImage->ModifyWebPAndUploadToStorage();
+    $webpImage->CreateModifiedWebPFromRequestBody();
+
+    // Process existing WMF image using given parameters
+    $wmfImage = new UpdateWmfImage($imagingApi);
+    $wmfImage->ModifyWmfFromStorage();
+    $wmfImage->ModifyWmfAndUploadToStorage();
+    $wmfImage->CreateModifiedWmfFromRequestBody();
+
+    // Update parameters of existing BMP image
+    $bmpImage = new UpdateBmpImage($imagingApi);
+    $bmpImage->ModifyBmpFromStorage();
+    $bmpImage->ModifyBmpAndUploadToStorage();
+    $bmpImage->CreateModifiedBmpFromRequestBody();
+
+    // AI APIs
+    echo "Running AI examples:" . PHP_EOL;
+
+    // Compare two images
+    $compareImages = new CompareImages($imagingApi);
+    $compareImages->PrepareSearchContext();
+    $compareImages->CompareTwoImagesInCloud();
+    $compareImages->CompareLoadedImageToImageInCloud();
+    $compareImages->DeleteSearchContext();
+
+    // Find duplicate images
+    $findDuplicateImages = new FindDuplicateImages($imagingApi);
+    $findDuplicateImages->PrepareSearchContext();
+    $findDuplicateImages->FindImageDuplicates();
+    $findDuplicateImages->DeleteSearchContext();
+
+    // Find similar images
+    $findSimilarImages = new FindSimilarImages($imagingApi);
+    $findSimilarImages->PrepareSearchContext();
+    $findSimilarImages->FindImagesSimilar();
+    $findSimilarImages->FindImagesByTag();
+    $findSimilarImages->DeleteSearchContext();
+} catch (\Exception $exception) {
+    echo "Something goes wrong: " . $exception . PHP_EOL;
+}
 
 function deleteDir($path)
 {
